8a63d6a14d37968395c4c5c95f0a2c16b0f20391
howellsy <jamie.howells@gmail.com>
2023-10-06 18:06:16 +0100

    Initial deploy script

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import { Script } from "forge-std/Script.sol";
+import { FundMe } from "../src/FundMe.sol";
+
+contract DeployFundMe is Script {
+  function run() external {
+    vm.startBroadcast();
+    new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    vm.stopBroadcast();
+  }
+}
