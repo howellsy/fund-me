742a70beccc3af02eaadcbe64dfa05393c7684fc
howellsy <jamie.howells@gmail.com>
2023-10-06 18:13:39 +0100

    Use deploy script to set up unit tests

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.18;
 
 import { Test, console } from "forge-std/Test.sol";
 import { FundMe } from "../src/FundMe.sol";
+import { DeployFundMe } from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
   FundMe fundMe;
 
   function setUp() external {
-    fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    DeployFundMe deployFundMe = new DeployFundMe();
+    fundMe = deployFundMe.run();
   }
 
   function testMinimumDollarsFive() public {
@@ -17,7 +19,7 @@ contract FundMeTest is Test {
   }
 
   function testOwnerIsMsgSender() public {
-    assertEq(fundMe.i_owner(), address(this));
+    assertEq(fundMe.i_owner(), msg.sender);
   }
 
   function testPriceFeedVersion() public {
