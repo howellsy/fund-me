fbfebdcc9ea79f29153bea659b793cecf3131346
howellsy <jamie.howells@gmail.com>
2023-10-10 14:04:51 +0100

    Add integration tests

---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import { Test, console } from "forge-std/Test.sol";
+import { FundMe } from "../../src/FundMe.sol";
+import { DeployFundMe } from "../../script/DeployFundMe.s.sol";
+import { FundFundMe, WithdrawFundMe } from "../../script/Interactions.s.sol";
+
+contract InteractionsTest is Test {
+  FundMe public fundMe;
+
+  address USER = makeAddr("user");
+  uint256 constant SEND_VALUE = 0.1 ether;
+  uint256 constant STARTING_BALANCE = 10 ether;
+
+  function setUp() external {
+    DeployFundMe deployFundMe = new DeployFundMe();
+    (fundMe, ) = deployFundMe.run();
+    vm.deal(USER, STARTING_BALANCE);
+  }
+
+  function testUserCanFundInteractions() public {
+    FundFundMe fundFundMe = new FundFundMe();
+    fundFundMe.fundFundMe(address(fundMe));
+
+    WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+    withdrawFundMe.withdrawFundMe(address(fundMe));
+
+    assert(address(fundMe).balance == 0);
+  }
+}
--- test/unit/FundMe.t.sol
+++ test/unit/FundMe.t.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.18;
 
 import { Test, console } from "forge-std/Test.sol";
-import { FundMe } from "../src/FundMe.sol";
-import { DeployFundMe } from "../script/DeployFundMe.s.sol";
+import { FundMe } from "../../src/FundMe.sol";
+import { DeployFundMe } from "../../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
   FundMe fundMe;
